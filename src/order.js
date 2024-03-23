dafc828cb11ed1ddab553084a577cf70a3e91fb1
Seong Kang <seong.h.kang2@gmail.com>
2024-03-23 11:13:26 -0500

    Finished order.js

--- src/order.js
+++ src/order.js
@@ -1,18 +1,46 @@
-// 1. adding new orders to an order type list
-// 1a. needs two inputs; order # and object array of orders.
-// 1b. needs to add whole object to array as orders will come in as objects
-// 1c. input = object; object
 
-
-function takeOrder(orderNumber, deliveryOrders) {
-  console.log()
-
+function takeOrder(orderNumber, ordersArray) {
+  if (ordersArray.length < 3) {
+    return ordersArray.push(orderNumber)
+  } 
 }
 
 
+function refundOrder(orderNumber, ordersArray) {
+  for (var i = 0; i < ordersArray.length; i++) {
+    if (ordersArray[i].orderNumber === orderNumber) {
+      ordersArray.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  var orderItems = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    orderItems.push(deliveryOrders[i].item)
+  }
+  return orderItems.join(", ")
+}
+
+
+
+function searchOrder(deliveryOrders, item){
+
+  for (var i = 0; i < deliveryOrders.length; i++) {
+
+    if (deliveryOrders[i].item === item) {
+    return true;
+   }
+   } 
+  return false
+   
+   }
+
+  
+
 module.exports = {
   takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  refundOrder,
+  listItems,
+  searchOrder
 }
